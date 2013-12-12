167810e4c02f866c6746fa822103852afa1663fd
Andy Shaw <c00lnerd314@gmail.com>
2013-12-11 21:08:02 -0500

    extend clean.py to state which files were cleaned

--- HillClimbing/clean.py
+++ HillClimbing/clean.py
@@ -1,11 +1,16 @@
 '''
 clean out all .pyc files
 '''
-
-import os
-files = os.listdir('.')
-for file in files:
-    if file[-4:] == '.pyc':
-        os.system('del ' + file)
-        
-print 'clean complete'
+def main():
+    '''clean out all .pyc files'''
+    import os
+    files = os.listdir('.')
+    for file in files:
+        if file[-4:] == '.pyc':
+            os.system('del ' + file)
+            print 'removed', file
+            
+    print 'clean complete'
+    
+if __name__ == '__main__':
+    main()
