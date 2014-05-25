afbc0612308626f2cf1f3747af0734d033fc7b52
Andy Shaw <c00lnerd314@gmail.com>
2014-05-24 22:09:12 -0400

    refactor hillclimbing algorithm

--- HillClimbing/hillClimbingAgent.py
+++ HillClimbing/hillClimbingAgent.py
@@ -16,8 +16,6 @@ returns: state that is a solution
 
 import heapq
 import array
-import boardScore as b
-import successor as s
 
 def hillClimbingAgent(boardSize, silent=False):
     #create initial state at random
@@ -26,17 +24,17 @@ def hillClimbingAgent(boardSize, silent=False):
     for i in range(boardSize):
         initial.append(random.randint(0, boardSize-1))
 
-    currNode = (b.boardScore(initial), s.copyArr(initial))
+    currNode = (boardScore(initial), copyArr(initial))
     i = 1
     while 1:
         childrenHeap = list()
         
         #find successors
-        children = s.successor(currNode[1])
+        children = successor(currNode[1])
         
         #rate each resulting board and add to heap
         for child in children:
-            heapq.heappush(childrenHeap, (b.boardScore(child), child))
+            heapq.heappush(childrenHeap, (boardScore(child), child))
         
         #pull out next best choice
         nextNode = heapq.heappop(childrenHeap)
@@ -65,3 +63,70 @@ def arrToString(arr):
     #drop last comma and return
     return s[:-1]
         
+'''
+The successor function will return a set of all the possible child states of
+the input state
+
+param: state array of row positions
+return: set of possible child states
+'''
+def successor(state):
+    import array
+    children = list()
+    
+    #for each column, the children will be the queen in every other row
+    #len(state) is one dimension of the board
+    for queen in range(len(state)):
+        for newPos in range(len(state)):
+            newState = copyArr(state)
+            
+            #skip if pos is the queen's current position
+            if newPos != state[queen]:
+                newState[queen] = newPos
+                children.append(newState)
+    
+    return children
+'''
+Copy array into a new array.
+Note: Python arr2 = arr1 will make equivalent by reference, this makes a new array
+
+param: arr1 array to be copied
+return: new identical array
+'''
+def copyArr(arr1):
+    import array
+    newArr = array.array(arr1.typecode)
+    for item in arr1:
+        newArr.append(item)
+    return newArr
+
+'''
+This method will take in a state, and score it based on how many queens are 
+attacking each other.
+param: state integer array holding state vector
+return: integer score or rating
+'''
+def boardScore(state):
+    attacks = 0
+    
+    #for each queen, see what it can attack
+    #don't check columns, the way the state is represented means 2 cannot be in 
+    #same column
+    for queen in range(len(state)):
+        #check any in same row
+        # for column in range(queen, len(state)):
+            # if state[queen] == state[column] and queen != column:
+                # attacks += 1
+
+        #check diagonals
+        #check after queen (from left to right)
+        offset = 0
+        pos = state[queen]
+        for column in range(queen+1, len(state)):
+            offset += 1
+            if pos+offset == state[column] or pos-offset == state[column]:
+                attacks += 1
+            if state[queen] == state[column] and queen != column:
+                attacks += 1
+            
+    return attacks
--- HillClimbing/testHarnessHillClimbing.py
+++ HillClimbing/testHarnessHillClimbing.py
@@ -10,9 +10,6 @@ harness will restart the agent until a correct solution is found.
 '''
 
 import sys
-import hillClimbingAgent as agent 
-import printBoard as p
-import boardScore as b
 import array
 
 def main(size, silent):
@@ -20,18 +17,170 @@ def main(size, silent):
     
     #run the agent until correct solution is found
     while 1:
-        answer = agent.hillClimbingAgent(size, silent)
+        answer = hillClimbingAgent(size, silent)
         
         #newline between each restart
         if not silent: print ''
         
         #check to see if solution is correct
-        if b.boardScore(answer) == 0:
+        if boardScore(answer) == 0:
             break
     
     #print solution
-    print agent.arrToString(answer)
-    p.printBoard(answer)
+    print arrToString(answer)
+    printBoard(answer)
+
+
+'''
+This agent is based off the Hill Climbing algorithm found on slide 5 of the Week04b-Optimization 
+slides.
+
+Note: Next choice is purely based on the heap's return algorithm.  In the case of a tie, it is not
+going to be random.
+
+param: boardsize integer board size
+returns: state that is a solution
+'''
+
+import heapq
+
+def hillClimbingAgent(boardSize, silent=False):
+    #create initial state at random
+    import random
+    initial = array.array('i')
+    for i in range(boardSize):
+        initial.append(random.randint(0, boardSize-1))
+
+    currNode = (boardScore(initial), copyArr(initial))
+    i = 1
+    while 1:
+        childrenHeap = list()
+        
+        #find successors
+        children = successor(currNode[1])
+        
+        #rate each resulting board and add to heap
+        for child in children:
+            heapq.heappush(childrenHeap, (boardScore(child), child))
+        
+        #pull out next best choice
+        nextNode = heapq.heappop(childrenHeap)
+        
+        #print status
+        if not silent: print 'Iteration:{0}\tState:<{1}>\tBest Child Score:{2}'.format(
+                        i, arrToString(currNode[1]), nextNode[0])
+        
+        #check if solution
+        if nextNode[0] == 0:
+            return nextNode[1]
+            
+        #if currNode is less than, then it is the best solution on the board, heap will assure that
+        if currNode[0] <= nextNode[0]:
+            return currNode[1]
+            
+        #try again on the nextNode
+        currNode = nextNode
+        i+=1
+        
+def arrToString(arr):
+    x = arr.tolist()
+    s = ''
+    for item in x:
+        s += str(item) + ','
+    #drop last comma and return
+    return s[:-1]
+        
+'''
+The successor function will return a set of all the possible child states of
+the input state
+
+param: state array of row positions
+return: set of possible child states
+'''
+def successor(state):
+    import array
+    children = list()
+    
+    #for each column, the children will be the queen in every other row
+    #len(state) is one dimension of the board
+    for queen in range(len(state)):
+        for newPos in range(len(state)):
+            newState = copyArr(state)
+            
+            #skip if pos is the queen's current position
+            if newPos != state[queen]:
+                newState[queen] = newPos
+                children.append(newState)
+    
+    return children
+'''
+Copy array into a new array.
+Note: Python arr2 = arr1 will make equivalent by reference, this makes a new array
+
+param: arr1 array to be copied
+return: new identical array
+'''
+def copyArr(arr1):
+    import array
+    newArr = array.array(arr1.typecode)
+    for item in arr1:
+        newArr.append(item)
+    return newArr
+
+'''
+This method will take in a state, and score it based on how many queens are 
+attacking each other.
+param: state integer array holding state vector
+return: integer score or rating
+'''
+def boardScore(state):
+    attacks = 0
+    
+    #for each queen, see what it can attack
+    #don't check columns, the way the state is represented means 2 cannot be in 
+    #same column
+    for queen in range(len(state)):
+        #check any in same row
+        # for column in range(queen, len(state)):
+            # if state[queen] == state[column] and queen != column:
+                # attacks += 1
+
+        #check diagonals
+        #check after queen (from left to right)
+        offset = 0
+        pos = state[queen]
+        for column in range(queen+1, len(state)):
+            offset += 1
+            if pos+offset == state[column] or pos-offset == state[column]:
+                attacks += 1
+            if state[queen] == state[column] and queen != column:
+                attacks += 1
+            
+    return attacks
+
+'''
+Using ASCII art, for an 30x30 board or less, display the positioning of the board
+'''
+def printBoard(state):
+    #catch the condition of the board being too large
+    if len(state) > 80:
+        return None
+    s = ''
+    #top line
+    s += '-'*(len(state)*2 + 1) + '\n'
+    #body
+    for row in range(len(state)):
+        s += '|'
+        for column in state:
+            if column == row:
+                s += 'X|'
+            else:
+                s += ' |'
+        s += '\n'
+    
+    #bottom line
+    s += '-'*(len(state)*2 + 1) + '\n'
+    print s
                 
 if __name__ == '__main__':
     #get board size as command line arg
